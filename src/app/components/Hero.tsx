456aa7b522998f07f0666386fb43f565c0c4056a
RezaGhasemi.info@gmail.com <RezaGhasemi.info@gmail.com>
2026-09-12 17:06:01 +0330

    Update Ramila website

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,11 +3,9 @@ import Image from "next/image";
 export default function Hero() {
 return ( <section className="relative">
 
-  {/* Hero Banner */}
+  {/* Desktop Hero */}
 
-  <div className="relative w-full h-auto md:h-[450px] aspect-[4/5] md:aspect-auto">
-
-    {/* Desktop Banner */}
+  <div className="hidden md:block relative w-full aspect-[2171/724]">
 
     <Image
       src="/hero-banner.png"
@@ -15,35 +13,14 @@ return ( <section className="relative">
       fill
       priority
       sizes="100vw"
-      className="hidden md:block object-cover object-center"
+      className="object-contain object-center"
     />
 
-    {/* Mobile Banner */}
+    {/* Desktop Buttons */}
 
-    <Image
-      src="/hero-mobile.png"
-      alt="Ramila Perfume"
-      fill
-      priority
-      sizes="100vw"
-      className="block md:hidden object-contain object-top"
-    />
+    <div className="absolute bottom-3 left-[34.5%] -translate-x-1/2">
 
-    {/* Buttons */}
-
-    <div
-      className="
-        absolute
-        bottom-1
-        left-[50%]
-        -translate-x-1/2
-        md:left-[31.5%]
-      "
-    >
-
-      <div className="flex flex-nowrap items-center justify-center gap-2 md:gap-5">
-
-        {/* مشاهده محصولات */}
+      <div className="flex flex-nowrap items-center justify-center gap-5">
 
         <a
           href="#products"
@@ -64,8 +41,6 @@ return ( <section className="relative">
         >
           مشاهده محصولات
         </a>
-
-        {/* واتساپ */}
 
         <a
           href="https://wa.me/989228595633"
@@ -89,8 +64,6 @@ return ( <section className="relative">
           سفارش در واتساپ
         </a>
 
-        {/* بله */}
-
         <a
           href="https://ble.ir/ramillabeautty"
           target="_blank"
@@ -102,6 +75,95 @@ return ( <section className="relative">
             px-3
             py-2
             text-[13px]
+            font-semibold
+            shadow-lg
+            transition
+            duration-300
+            rounded-full
+            whitespace-nowrap
+          "
+        >
+          سفارش در بله
+        </a>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  {/* Mobile Hero */}
+
+  <div className="block md:hidden relative w-full aspect-[4/5]">
+
+    <Image
+      src="/hero-mobile.png"
+      alt="Ramila Perfume"
+      fill
+      priority
+      sizes="100vw"
+      className="object-contain object-top"
+    />
+
+    {/* Mobile Buttons */}
+
+    <div className="absolute bottom-1 left-1/2 -translate-x-1/2">
+
+      <div className="flex flex-nowrap items-center justify-center gap-2">
+
+        <a
+          href="#products"
+          className="
+            bg-[#173F2E]
+            hover:bg-[#0F2D21]
+            text-white
+            px-3
+            py-2
+            text-[12px]
+            font-semibold
+            shadow-lg
+            transition
+            duration-300
+            rounded-full
+            whitespace-nowrap
+          "
+        >
+          مشاهده محصولات
+        </a>
+
+        <a
+          href="https://wa.me/989228595633"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="
+            bg-[#25D366]
+            hover:bg-[#1EBE5D]
+            text-white
+            px-3
+            py-2
+            text-[12px]
+            font-semibold
+            shadow-lg
+            transition
+            duration-300
+            rounded-full
+            whitespace-nowrap
+          "
+        >
+          سفارش در واتساپ
+        </a>
+
+        <a
+          href="https://ble.ir/ramillabeautty"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="
+            bg-[#173F2E]
+            hover:bg-[#0F2D21]
+            text-white
+            px-3
+            py-2
+            text-[12px]
             font-semibold
             shadow-lg
             transition
